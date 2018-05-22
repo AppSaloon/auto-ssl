20a7a7538164c318bef066f605e65391302f2aa3
david <david@appsaloon.be>
2018-05-22 10:20:33 +0200

    corrected main.sh relative paths

--- bin/main.sh
+++ bin/main.sh
@@ -8,13 +8,13 @@ echo "this is the postinstall script for auto-ssl-on-express-with-docker"
 echo "The following files will be copied to your project directory: letsencrypt_webhook.sh Dockerfile"
 echo "If Dockerfile already exists, the new one will be renamed to Dockerfile.new. Please merge it with your old Dockerfile."
 
-cp -v "./bin/letsencrypt_webroot.sh" "../../"
+cp -v "./bin/letsencrypt_webroot.sh" "../../../"
 
-if [ -f ../../Dockerfile ]; then
-    cp -v "./bin/letsencrypt_Dockerfile" "../../Dockerfile.new"
+if [ -f ../../../Dockerfile ]; then
+    cp -v "./bin/letsencrypt_Dockerfile" "../../../Dockerfile.new"
     echo "Dockerfile already existed. Merge Dockerfile.new with your existing Dockerfile."
 else
-    cp -v "./bin/letsencrypt_Dockerfile" "../../Dockerfile"
+    cp -v "./bin/letsencrypt_Dockerfile" "../../../Dockerfile"
 fi
 
 echo  "auto-ssl-on-express-with-docker postinstall script: done."
